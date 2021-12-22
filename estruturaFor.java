ce76ee03af086fd40191fa9aafcef96dfa2ddaa3
unknown <dleitedias@gmail.com>
2021-12-22 08:23:37 -0300

    Praticando estrutura repetitiva FOR

---
+++ estruturaFor.java
@@ -0,0 +1,29 @@
+package praticando;
+
+import java.util.Scanner;
+
+public class estruturaFor {
+
+	public static void main(String[] args) {
+		
+	/*Fazer um programa que lê um valor inteiro N e depois N números inteiros.
+      Ao final, mostra a soma dos N números lidos
+	 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int soma = 0;
+ 		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma = soma + x;
+		}
+		
+ 		System.out.println(soma);
+ 		
+		sc.close();
+
+	}
+
+}
