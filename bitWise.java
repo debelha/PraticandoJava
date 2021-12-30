0f9dd0cd625bfb2680d343c06f742d084bc920cd
unknown <dleitedias@gmail.com>
2021-12-30 09:28:22 -0300

    Praticando BitWise

---
+++ bitWise.java
@@ -0,0 +1,15 @@
+package praticando;
+
+public class bitWise {
+
+	public static void main(String[] args) {
+		
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+
+	}
+
+}
